d07b06a8a65cb8b1cbf424eefa996507051e7976
sys_cat <systemcat91@gmail.com>
2016-03-16 21:16:44 +0900

    add use MeCab Engine.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Kinsokujiko
 Mask ng-word powered by MeCab
+
+## Description
+
+using MeCab.Please install MeCab.
+
+
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "fmt"
+  "github.com/yukihir0/mecab-go"
+)
+
+func main() {
+  input := "testをTestと認識出来ない奴はクソッタレだ。"
+
+  args := mecab.NewArgs()
+  args.DicDir = "/usr/local/lib/mecab/dic/mecab-ipadic-neologd"
+  parser, err := mecab.InitializeParser(args)
+  if err != nil {
+    panic(err)
+  }
+  defer parser.Release()
+  nodes, err := parser.Parse(input)
+  if err != nil {
+    panic(err)
+  }
+  for _, node := range nodes {
+    fmt.Println(node)
+    /*
+    if node.Pos == "名詞" && (node.Pos1 == "一般" || node.Pos1 == "固有名詞") {
+      fmt.Println(node.Surface)
+    }
+    */
+  }
+}
