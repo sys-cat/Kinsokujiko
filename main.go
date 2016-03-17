271756e9cd6fcf2ad319b463c01e68e8bf77f5c2
sys_cat <systemcat91@gmail.com>
2016-03-17 16:13:13 +0900

    run from command line

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
   "fmt"
+  "flag"
   "github.com/yukihir0/mecab-go"
 )
 
 func main() {
-  input := "testをTestと認識出来ない奴はクソッタレだ。"
+  var input string
+  flag.StringVar(&input, "input", "", "Of the analyzed text")
+  flag.Parse()
 
   args := mecab.NewArgs()
   args.DicDir = "/usr/local/lib/mecab/dic/mecab-ipadic-neologd"
