3b0418069a95cb7460f15fc17806f071acf04a03
sys_cat <systemcat91@gmail.com>
2016-03-25 12:59:14 +0900

    initialize main.go

--- main.go
+++ main.go
@@ -2,32 +2,7 @@ package main
 
 import (
   "fmt"
-  "flag"
-  //"github.com/yukihir0/mecab-go"
 )
 
 func main() {
-  var input string
-  flag.StringVar(&input, "input", "", "Of the analyzed text")
-  flag.Parse()
-
-  args := mecab.NewArgs()
-  args.DicDir = "/usr/local/lib/mecab/dic/mecab-ipadic-neologd"
-  parser, err := mecab.InitializeParser(args)
-  if err != nil {
-    panic(err)
-  }
-  defer parser.Release()
-  nodes, err := parser.Parse(input)
-  if err != nil {
-    panic(err)
-  }
-  for _, node := range nodes {
-    fmt.Println(node)
-    /*
-    if node.Pos == "名詞" && (node.Pos1 == "一般" || node.Pos1 == "固有名詞") {
-      fmt.Println(node.Surface)
-    }
-    */
-  }
 }
