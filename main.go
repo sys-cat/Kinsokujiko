c67c8f1ba4656aaa0b92c2bc5cb9f56121d5f179
sys-cat <systemcat91@gmail.com>
2016-03-28 04:56:11 +0900

    add POST Request and Request Paramaters Test.

--- main.go
+++ main.go
@@ -2,14 +2,31 @@ package main
 
 import (
   "github.com/gin-gonic/gin"
+  //"fmt"
 )
 
+type GetListName struct {
+  Value string `json:"key"`
+  //Text string `json:text`
+}
+
 func main() {
-  r := gin.Default()
-  r.GET("/ping", func(c *gin.Context) {
-    c.JSON(200, gin.H{
-      "message":"pong",
-    })
+  router := gin.Default()
+
+  router.POST("/list", func(c *gin.Context) {
+    var val GetListName
+    c.Bind(&val)
+    //c.String(200, val.Value)
+    c.JSON(200, val)
   })
-  r.Run()
+  router.Run(":3000")
+}
+
+func getBlackList(c *gin.Context) {
+  var val GetListName
+  c.Bind(&val)
+  c.JSON(200, val.Value)
+/*  c.JSON(200, gin.H{
+    "return":val.Value,
+  })*/
 }
