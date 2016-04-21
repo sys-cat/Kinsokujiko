6c6580b48b9a0afb5e665f71b1517b9364ae587e
sys_cat <systemcat91@gmail.com>
2016-04-21 18:32:08 +0900

    modify for CORS

--- main.go
+++ main.go
@@ -41,8 +41,7 @@ func main() {
 }
 
 func setAccessHeader(c *gin.Context) {
-  c.Header("Access-Control-Allow-Origin", "http://localhost")
-  c.Header("Access-Control-Allow-Credentials", "true")
+  c.Header("Access-Control-Allow-Origin", "*")
   c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
   c.Header("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS")
 }
