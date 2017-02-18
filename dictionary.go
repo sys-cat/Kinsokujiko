8c34f33fa5f3a869619d32057e6aa0bdb892028b
sys-cat <systemcat91@gmail.com>
2017-02-18 19:26:59 +0900

    add methods for dictionary function.

--- dictionary.go
+++ dictionary.go
@@ -1,5 +1,12 @@
 package Kinsokujiko
 
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
+
 // Item is dictionary Item
 type Item struct {
 	Surf  string
@@ -11,4 +18,49 @@ type Item struct {
 // Dictionary is Slice any Item
 type Dictionary []Item
 
-var dicPaht = "_dic/dic.txt"
+var dicPath = "_dic/dic.txt"
+
+func Update(dic Dictionary) (bool, error) {
+	_, err := _add_item(dic)
+	if err != nil {
+		fmt.Print(err)
+	}
+	for index, item := range dic {
+		if !_check_item(item) {
+			return false, errors.New(fmt.Sprintf("%d 行目に問題があるようです", index))
+		}
+	}
+	return _add_item(dic)
+}
+
+func _check_item(it Item) bool {
+	var res = false
+	if strings.Count(it.Surf, "") > 1 {
+		res = true
+	}
+	if strings.Count(it.Slice, "") > 1 {
+		res = true
+	}
+	if strings.Count(it.Kana, "") > 1 {
+		res = true
+	}
+	if strings.Count(it.Pos, "") > 1 {
+		res = true
+	}
+	return res
+}
+
+func _add_item(dic Dictionary) (bool, error) {
+	file, err := os.OpenFile(dicPath, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+	}
+	defer file.Close()
+	fmt.Sprintf("ファイルの詳細：%+v", file)
+	return true, nil
+}
+
+func Show() (Dictionary, error) {
+	return Dictionary{}, nil
+}
+
+func _get_dic() {}
