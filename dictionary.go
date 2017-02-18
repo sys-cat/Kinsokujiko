a1969fbe52ebc70b81ed6d5f57d619765167354e
sys-cat <systemcat91@gmail.com>
2017-02-19 04:07:42 +0900

    delete extra comment.

--- dictionary.go
+++ dictionary.go
@@ -11,10 +11,10 @@ import (
 
 // Item is dictionary Item
 type Item struct {
-	Surf  string `json:"surf" form:"surf"`
-	Slice string `json:"slice" form:"slice"`
-	Kana  string `json:"kana" form:"kana"`
-	Pos   string `json:"pos" form:"pos"`
+	Surf  string
+	Slice string
+	Kana  string
+	Pos   string
 }
 
 // Dictionary is Slice any Item
